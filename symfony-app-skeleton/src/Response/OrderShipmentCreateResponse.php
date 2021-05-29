7873e4f96d386137e35df3e800c92c62ae734093
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-05-30 02:50:55 +0300

    shipment order Create, Get, and Update APIs were being added

---
+++ symfony-app-skeleton/src/Response/OrderShipmentCreateResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Response;
+
+class OrderShipmentCreateResponse
+{
+    public $clientUserID;
+
+    public $transportationType;
+
+    public $target;
+
+    public $supplierID;
+
+    public $distributorID;
+
+    public $exportWarehouseID;
+
+    public $quantity;
+
+    public $image;
+
+    public $productCategoryID;
+
+    public $unit;
+
+    public $receiverName;
+
+    public $receiverPhoneNumber;
+
+    public $paymentTime;
+
+    public $vehicleIdentificationNumber;
+
+    public $extraSpecification;
+
+    public $status;
+}
