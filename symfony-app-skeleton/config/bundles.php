b9ffe70893b91c041221d407d4f7083483ffe491
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-06-06 15:00:19 +0300

    file upload system was being added

--- symfony-app-skeleton/config/bundles.php
+++ symfony-app-skeleton/config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
