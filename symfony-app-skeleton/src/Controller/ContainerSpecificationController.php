f2ff2fb58a5b29813523a4408267ce35391c20b9
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-06-20 20:17:22 +0300

    Create, Get all, and Delete container specification APIs were being added

---
+++ symfony-app-skeleton/src/Controller/ContainerSpecificationController.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Controller;
+
+use App\AutoMapping;
+use App\Request\ContainerSpecificationCreateRequest;
+use App\Request\DeleteRequest;
+use App\Service\ContainerSpecificationService;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
+use stdClass;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ContainerSpecificationController extends BaseController
+{
+    private $autoMapping;
+    private $validator;
+    private $containerSpecificationService;
+
+    public function __construct(SerializerInterface $serializer, AutoMapping $autoMapping, ValidatorInterface $validator, ContainerSpecificationService $containerSpecificationService)
+    {
+        parent::__construct($serializer);
+
+        $this->autoMapping = $autoMapping;
+        $this->validator = $validator;
+        $this->containerSpecificationService = $containerSpecificationService;
+    }
+
+    /**
+     * @Route("containerspecification", name="createContainerSpecification", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     * 
+     * @OA\Tag(name="Container Specification")
+     * 
+     * @OA\RequestBody(
+     *      description="Create new container",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="type"),
+     *          @OA\Property(type="integer", property="providedBy"),
+     *          @OA\Property(type="number", property="capacityCPM", description="of type Float"),
+     *          @OA\Property(type="number", property="widthInMeter", description="of type Float"),
+     *          @OA\Property(type="number", property="hightInMeter", description="of type Float"),
+     *          @OA\Property(type="number", property="lengthInMeter", description="of type Float")
+     *      )
+     * )
+     * 
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns the creation date of the new specifications",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="object", property="Data",
+     *                  @OA\Property(type="object", property="createdAt")
+     *          )
+     *      )
+     * )
+     * 
+     * @Security(name="Bearer")
+     */
+    public function create(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $request = $this->autoMapping->map(stdClass::class, ContainerSpecificationCreateRequest::class, (object)$data);
+
+        $request->setCreatedBy($this->getUserId());
+
+        $violations = $this->validator->validate($request);
+
+        if (\count($violations) > 0)
+        {
+            $violationsString = (string) $violations;
+
+            return new JsonResponse($violationsString, Response::HTTP_OK);
+        }
+
+        $result = $this->containerSpecificationService->create($request);
+
+        return $this->response($result, self::CREATE);
+    }
+
+    /**
+     * @Route("containerspecifications", name="getAllContainerSpecification", methods={"GET"})
+     * @param Request $request
+     * @return JsonResponse
+     * 
+     * @OA\Tag(name="Container Specification")
+     * 
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns the creation date of the new specifications",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="array", property="Data",
+     *              @OA\Items(
+     *                  @OA\Property(type="integer", property="id"),
+     *                  @OA\Property(type="string", property="type"),
+     *                  @OA\Property(type="number", property="subcontractName"),
+     *                  @OA\Property(type="number", property="capacityCPM"),
+     *                  @OA\Property(type="number", property="widthInMeter"),
+     *                  @OA\Property(type="number", property="hightInMeter"),
+     *                  @OA\Property(type="object", property="lengthInMeter"),
+     *                  @OA\Property(type="object", property="createdAt"),
+     *                  @OA\Property(type="object", property="updatedAt"),
+     *                  @OA\Property(type="string", property="createdByUser"),
+     *                  @OA\Property(type="string", property="createdByUserImage"),
+     *                  @OA\Property(type="string", property="updatedByUser"),
+     *                  @OA\Property(type="string", property="updatedByUserImage")
+     *              )
+     *          )
+     *      )
+     * )
+     * 
+     */
+    public function getAllContainerSpecifications()
+    {
+        $result = $this->containerSpecificationService->getAllContainerSpecifications();
+
+        return $this->response($result, self::FETCH);
+    }
+
+    /**
+     * @Route("containerspecification/{id}", name="deleteContainerSpecification", methods={"DELETE"})
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @OA\Tag(name="Container Specification")
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns the info of the deleted container specification",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="array", property="Data",
+     *              @OA\Items(
+     *                  @OA\Property(type="integer", property="id"),
+     *                  @OA\Property(type="string", property="type"),
+     *                  @OA\Property(type="number", property="subcontractName"),
+     *                  @OA\Property(type="number", property="capacityCPM"),
+     *                  @OA\Property(type="number", property="widthInMeter"),
+     *                  @OA\Property(type="number", property="hightInMeter"),
+     *                  @OA\Property(type="object", property="lengthInMeter"),
+     *                  @OA\Property(type="object", property="createdAt"),
+     *                  @OA\Property(type="object", property="updatedAt"),
+     *                  @OA\Property(type="string", property="createdByUser"),
+     *                  @OA\Property(type="string", property="createdByUserImage"),
+     *                  @OA\Property(type="string", property="updatedByUser"),
+     *                  @OA\Property(type="string", property="updatedByUserImage")
+     *              )
+     *          )
+     *      )
+     * )
+     * 
+     */
+    public function deleteContainerSpecificationById(Request $request)
+    {
+        $request = new DeleteRequest($request->get('id'));
+
+        $result = $this->containerSpecificationService->deleteContainerSpecificationById($request);
+
+        return $this->response($result, self::DELETE);
+    }
+
+}
---
+++ symfony-app-skeleton/src/Service/ContainerSpecificationService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Service;
+
+use App\AutoMapping;
+use App\Entity\ContainerSpecificationEntity;
+use App\Manager\ContainerSpecificationManager;
+use App\Request\ContainerSpecificationCreateRequest;
+use App\Response\ContainerSpecificationCreateResponse;
+use App\Response\ContainerSpecificationGetResponse;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class ContainerSpecificationService
+{
+    private $autoMapping;
+    private $containerSpecificationManager;
+    private $params;
+
+    public function __construct(AutoMapping $autoMapping, ContainerSpecificationManager $containerSpecificationManager, ParameterBagInterface $params)
+    {
+        $this->autoMapping = $autoMapping;
+        $this->containerSpecificationManager = $containerSpecificationManager;
+        $this->params = $params->get('upload_base_url') . '/';
+    }
+
+    public function create(ContainerSpecificationCreateRequest $request)
+    {
+        $containerSpecificationResult = $this->containerSpecificationManager->create($request);
+
+        return $this->autoMapping->map(ContainerSpecificationEntity::class, ContainerSpecificationCreateResponse::class, $containerSpecificationResult);
+    }
+
+    public function getAllContainerSpecifications()
+    {
+        $containerSpecificationResponse = [];
+
+        $specifications = $this->containerSpecificationManager->getAllContainerSpecifications();
+
+        foreach ($specifications as $specification)
+        {
+            if($specification['createdByUserImage'])
+            {
+                $specification['createdByUserImage'] = $this->params . $specification['createdByUserImage'];
+            }
+
+            if($specification['updatedByUserImage'])
+            {
+                $specification['updatedByUserImage'] = $this->params . $specification['updatedByUserImage'];
+            }
+
+            $containerSpecificationResponse[] = $this->autoMapping->map('array', ContainerSpecificationGetResponse::class, $specification);
+        }
+
+        return $containerSpecificationResponse;
+    }
+
+    public function deleteContainerSpecificationById($request)
+    {
+        $result = $this->containerSpecificationManager->deleteContainerSpecificationById($request);
+
+        return $this->autoMapping->map(ContainerSpecificationEntity::class, ContainerSpecificationGetResponse::class, $result);
+    }
+
+}
