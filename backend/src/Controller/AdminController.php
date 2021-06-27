6115f1152124ed02341e4b59486344358c5616f8
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-06-28 00:29:48 +0300

    OpenAPI annotations were being added

--- backend/src/Controller/AdminController.php
+++ backend/src/Controller/AdminController.php
@@ -6,6 +6,8 @@ use App\AutoMapping;
 use App\Request\AdminCreateRequest;
 use App\Request\DeleteRequest;
 use App\Service\AdminService;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 use stdClass;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,6 +34,38 @@ class AdminController extends BaseController
      * @Route("createadmin", name="adminCreate", methods={"POST"})
      * @param Request $request
      * @return JsonResponse
+     *
+     *
+     * @OA\Tag(name="Admin")
+     *
+     * @OA\RequestBody(
+     *      description="Creates admin/employee and profile at the same time",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="userID"),
+     *          @OA\Property(type="string", property="password"),
+     *          @OA\Property(type="string", property="userName"),
+     *          @OA\Property(type="string", property="email"),
+     *          @OA\Property(type="array", property="roles", description="set it when create employee",
+     *              @OA\Items(example="ROLE_EMPLOYEE"))
+     *              )
+     *          )
+     *      )
+     * )
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns the new admin's role",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="object", property="Data",
+     *                  @OA\Property(type="array", property="roles",
+     *                      @OA\Items(example="user")),
+     *                  @OA\Property(type="object", property="createdAt")
+     *          )
+     *      )
+     * )
+     *
      */
     public function adminCreate(Request $request)
     {
@@ -54,6 +88,33 @@ class AdminController extends BaseController
 
     /**
      * @Route("adminprofile", name="getAdminProfileByUserID", methods={"GET"})
+     *
+     *
+     * @OA\Tag(name="Admin")
+     *
+     * @OA\Parameter(
+     *      name="token",
+     *      in="header",
+     *      description="token to be passed as a header",
+     *      required=true
+     * )
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns the profile of the signed-in admin/employee",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="object", property="Data",
+     *                  @OA\Property(type="string", property="userName"),
+     *                  @OA\Property(type="string", property="image"),
+     *                  @OA\Property(type="string", property="phone")
+     *          )
+     *      )
+     * )
+     *
+     * @Security(name="Bearer")
+     *
      */
     public function getProfileByUserID()
     {
@@ -65,6 +126,25 @@ class AdminController extends BaseController
     /**
      * @Route("employees", name="getAllEmployees", methods={"GET"})
      * @return JsonResponse
+     *
+     * @OA\Tag(name="Admin")
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns all employees",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="array", property="Data",
+     *              @OA\Items(
+     *                  @OA\Property(type="string", property="userName"),
+     *                  @OA\Property(type="string", property="image"),
+     *                  @OA\Property(type="string", property="phone")
+     *              )
+     *          )
+     *      )
+     * )
+     *
      */
     public function getAllEmployees()
     {
@@ -76,6 +156,25 @@ class AdminController extends BaseController
     /**
      * @Route("administrators", name="getAllAdministrators", methods={"GET"})
      * @return JsonResponse
+     *
+     * @OA\Tag(name="Admin")
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns all administrators",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="array", property="Data",
+     *              @OA\Items(
+     *                  @OA\Property(type="string", property="userName"),
+     *                  @OA\Property(type="string", property="image"),
+     *                  @OA\Property(type="string", property="phone")
+     *              )
+     *          )
+     *      )
+     * )
+     *
      */
     public function getAllAdministrators()
     {
@@ -88,6 +187,23 @@ class AdminController extends BaseController
      * @Route("admin/{id}", name="deleteAdmin", methods={"DELETE"})
      * @param Request $request
      * @return JsonResponse
+     *
+     * @OA\Tag(name="Admin")
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns the info of the deleted admin/employee",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="object", property="Data",
+     *                  @OA\Property(type="array", property="roles",
+     *                      @OA\Items(example="user")),
+     *                  @OA\Property(type="object", property="createdAt")
+     *          )
+     *      )
+     * )
+     *
      */
     public function deleteAdminById(Request $request)
     {
