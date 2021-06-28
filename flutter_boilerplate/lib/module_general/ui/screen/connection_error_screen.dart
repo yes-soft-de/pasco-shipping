09369a250001d8f34f7b3b15f5f42de6502a6fa0
Rahaft <rahaftanous1@gmail.com>
2021-06-28 03:57:56 +0300

    added general module that contains general screen like (connection error OR empty data....)

---
+++ flutter_boilerplate/lib/module_general/ui/screen/connection_error_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pasco_shipping/utils/styles/static_images.dart';
+import 'package:pasco_shipping/utils/styles/text_style.dart';
+
+class ErrorScreen extends StatelessWidget {
+  final String error;
+  final Function retry;
+  const ErrorScreen({required this.error, required this.retry});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Image.asset(StaticImage.noInternet), Text(error , style: white18text,)],
+    );
+  }
+}
