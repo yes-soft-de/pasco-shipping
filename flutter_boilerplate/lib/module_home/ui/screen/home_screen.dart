cf0af7be1984d59baba1cea04e3527cdf8686635
Rahaft <rahaftanous1@gmail.com>
2021-07-01 03:48:06 +0300

    add permission for request newshipment

--- flutter_boilerplate/lib/module_home/ui/screen/home_screen.dart
+++ flutter_boilerplate/lib/module_home/ui/screen/home_screen.dart
@@ -1,13 +1,18 @@
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pasco_shipping/generated/l10n.dart';
 import 'package:pasco_shipping/module_home/ui/widget/chip_card.dart';
 import 'package:pasco_shipping/module_mark/mark_routes.dart';
+import 'package:pasco_shipping/module_my_shipment/my_shipment_routes.dart';
 import 'package:pasco_shipping/module_profile/profile_routes.dart';
 import 'package:pasco_shipping/module_shipment_previous/previous_routes.dart';
-import 'package:pasco_shipping/module_shipment_previous/ui/widget/search_card.dart';
+import 'package:pasco_shipping/module_shipment_request/presistance/shipment_prefs_helper.dart';
+import 'package:pasco_shipping/module_shipment_track/tracking_routes.dart';
+import 'package:pasco_shipping/module_shipment_track/ui/widget/search_card.dart';
 import 'package:pasco_shipping/module_shipment_request/request_routes.dart';
+import 'package:pasco_shipping/module_theme/service/theme_service/theme_service.dart';
 import 'package:pasco_shipping/utils/styles/static_images.dart';
 import 'package:pasco_shipping/utils/widget/background.dart';
 
@@ -19,11 +24,15 @@ class HomeScreen extends StatelessWidget {
     ScrollController controller = ScrollController();
 
     return Background(
+      goBack: (){},
       controller: controller,
       isHome: true,
         child: Column(
           children: [
-            SearchCard(),
+            SearchCard(onSearch: (trackNumber){
+              Navigator.pushNamed(context,
+                TracingRoutes.TRACKING_SCREEN , arguments: {'trackNumber': trackNumber}, );
+            },),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -37,14 +46,22 @@ class HomeScreen extends StatelessWidget {
                 ChipCard(
                   icon: StaticImage.box,
                   onPressed: () {
-                    Navigator.pushNamed(context, PreviousShipmentsRoutes.TRACKING_SHIPMENTS);
+                    Navigator.pushNamed(context, MyShipmentRoutes.MY_SHIPMENT);
                   },
                   label: S.of(context).myShipment,
                 ),
                 ChipCard(
                   icon: StaticImage.newShipments,
-                  onPressed: () {
-                    Navigator.pushNamed(context, NewShipmentRoutes.NEW_SHIPMENTS);
+                  onPressed: () async {
+
+                      await getShipment().then((value) {
+                        if(value == null || value =='null'){
+                          Navigator.pushNamed(context, NewShipmentRoutes.NEW_SHIPMENTS);
+                          print('value null');
+                        }else {
+                          _showDialog(context);
+                        }
+                      });
                   },
                   label: S.of(context).newShipmentRequest,
                 ),
@@ -99,6 +116,20 @@ class HomeScreen extends StatelessWidget {
         title: S.of(context).shipment,
       currentIndex: 0,
       isResultScreen: false,
+    );
+  }
+
+  void _showDialog(BuildContext context) {
+    // flutter defined function
+    CoolAlert.show(
+      context: context,
+      type: CoolAlertType.warning,
+      text: 'You have a shipment waiting for confirmation, please confirm it first and then order a new shipment',
+      backgroundColor:AppThemeDataService.PrimaryColor,
+      confirmBtnColor:AppThemeDataService.AccentColor,
+      onConfirmBtnTap: (){
+        Navigator.pop(context);
+      },
     );
   }
 }
