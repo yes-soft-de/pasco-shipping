e933d5d290dd862fedcb535c44adf8eeb4483282
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-07-13 02:40:17 +0300

    shipperID and consigneeID added to container entity

--- backend/src/Request/ContainerCreateRequest.php
+++ backend/src/Request/ContainerCreateRequest.php
@@ -14,6 +14,10 @@ class ContainerCreateRequest
 
     private $providedBy;
 
+    private $shipperID;
+
+    private $consigneeID;
+
     private $createdBy;
 
     public function setCreatedBy($userID)
