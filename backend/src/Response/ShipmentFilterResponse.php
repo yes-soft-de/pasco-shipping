accf2fa32606981ef9f7cfff89d70c5259559396
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-08-08 17:19:35 +0300

    filteracceptedshipments API response updated

--- backend/src/Response/ShipmentFilterResponse.php
+++ backend/src/Response/ShipmentFilterResponse.php
@@ -56,6 +56,10 @@ class ShipmentFilterResponse
 
     public $status;
 
+    public $externalWarehouseInfo;
+
+    public $isExternalWarehouse;
+
     public $orderUpdatedByUser;
 
     public $orderUpdatedByUserImage;
