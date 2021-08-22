da8233f630af97da107f8aa31a54263dc0051453
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-08-22 20:57:56 +0300

    kreait/firebase bundle was installed

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
 ];
